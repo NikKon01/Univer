8d1cf88a929dd8e2a0db33be753bb42dcafd265d
NikKon01 <4453431@gmail.com>
2022-09-20 12:24:12 +0300

    new

--- Tasks/Rand.py
+++ Tasks/Rand.py
@@ -1,28 +1,32 @@
 import random
-r1 = random.randint(0, 100)
+r1 = random.randint(0, 50)
 print(r1)
-r2 = random.randint(0, 100)
+r2 = random.randint(0, 50)
 print(r2)
 r = r1 + r2
 print(r)
+Li = 3
 
 while True:
-    answer = input("Введите число: ")
+    answer = input("Введите число от 0 до 100: ")
     if not answer.isdigit():
         print("Введите число!")
         continue
     answer = int(answer)
-    live = 3
     if answer > r:
-        live = live - 1
-        print("Загаданное число меньше")
+        Li = Li - 1
+        if Li == 0:
+            print("Вы проиграли!")
+            break
+        else:
+            print(f"Загаданное число меньше, осталось {Li} попытки")
     elif answer < r:
-        live = live - 1
-        print("Загаданное число больше")
-    elif live = 0:
-        print("Вы проиграли!")
-        break
+        Li = Li - 1
+        if Li == 0:
+            print("Вы проиграли!")
+            break
+        else:
+            print(f"Загаданное число больше, осталось {Li} попытки")
     else:
         print("Вы угадали!")
         break
-
