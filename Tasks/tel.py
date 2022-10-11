7770a6a7bfe02b47dc7235012d96e4ff46404c63
NikKon01 <4453431@gmail.com>
2022-10-11 12:14:20 +0300

    new

--- Tasks/tel.py
+++ Tasks/tel.py
@@ -2,29 +2,44 @@ d = {}
 
 while True:
     print("Добро пожаловать! Введите команды:\n0 - завершить программу\n1 - добавить контакат\n2 - удалить контакт\n3 - просмотреть телефонную книгу\n4 - изменить номер телефона")
-    commands = input()
+    commands = int(input())
 
     if (commands < 0) and (commands > 4):
         print("Неверно введена команда!")
         continue
 
-    if commands == 0:
+    elif commands == 0:
         print("Завершение программы...")
         break
 
-    if commands == 1:
-        a = input()
-        a.title()
-        b = input()
+    elif commands == 1:
+        def get_num():
 
-        d[a] = b
+            num = input("Введите номер телефона: ")
+            num = num.replace(" ", "").replace("-", "")
+
+            if (num[0] == "8") and (len(num) == 11):
+                num = num.replace("8", "+7", 1)
+            if (num[0] == "7") and (len(num) == 11):
+                num = "+" + num
+            if (num[0] == "9") and (len(num) == 10):
+                num = "+7" + num
+            if (len(num) == 12) and (num[1:].isdigit()):
+                return(num)
+            else:
+                print("Неправильно набран номер!")
+                return get_num()
+        def get_name():
+            name = input("Введите имя: ")
+            name = name.title()
+            return name
+
+        d[get_name] = get_num
 
         c = input("Всё? y/n")
+            if c == 'y':
+                break
 
-        if c == 'y':
-            break
-
-        if commands == 2:
 
 
 
