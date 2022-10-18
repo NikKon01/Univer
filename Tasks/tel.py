dfeb9298193d5f5cc2ac34ae08e6ca90892a30d4
NikKon01 <4453431@gmail.com>
2022-10-18 11:18:42 +0300

    new

--- Tasks/tel.py
+++ Tasks/tel.py
@@ -1,47 +1,51 @@
-d = {}
+def get_num():
+    num = input("Введите номер телефона: ")
+    num = num.replace(" ", "").replace("-", "")
+
+    if (num[0] == "8") and (len(num) == 11):
+        num = num.replace("8", "+7", 1)
+    if (num[0] == "7") and (len(num) == 11):
+        num = "+" + num
+    if (num[0] == "9") and (len(num) == 10):
+        num = "+7" + num
+    if (len(num) == 12) and (num[1:].isdigit()):
+        return(num)
+    else:
+        print("Неправильно набран номер!")
+        return get_num()
+
+def get_name():
+    name = input("Введите имя: ")
+    name = name.title()
+    return name
+
+def get_contact(dict, names, num):
+    dict[names] = num
+    print("Контакт добавлен")
+    return dict
+
+def remove_contact (dict, names)
+    if not (names in dict):
+        print("Нет такого номера!")
+        return False
+    del dict[names]
+    return dict
+
+def show_dict(dict):
+    print("Список контактов")
+    for i in dict:
+        print(i, dict[i])
+
+def change_num(dict, names):
+    if not (names in dict):
+        print("Нет такого номера!")
+        return False
+    dict[names] = get_num()
+    print("Контакт изменён")
+    return(dict)
+
+i = 0
+dict = {}
 
 while True:
-    print("Добро пожаловать! Введите команды:\n0 - завершить программу\n1 - добавить контакат\n2 - удалить контакт\n3 - просмотреть телефонную книгу\n4 - изменить номер телефона")
-    commands = int(input())
-
-    if (commands < 0) and (commands > 4):
-        print("Неверно введена команда!")
-        continue
-
-    elif commands == 0:
-        print("Завершение программы...")
-        break
-
-    elif commands == 1:
-        def get_num():
-
-            num = input("Введите номер телефона: ")
-            num = num.replace(" ", "").replace("-", "")
-
-            if (num[0] == "8") and (len(num) == 11):
-                num = num.replace("8", "+7", 1)
-            if (num[0] == "7") and (len(num) == 11):
-                num = "+" + num
-            if (num[0] == "9") and (len(num) == 10):
-                num = "+7" + num
-            if (len(num) == 12) and (num[1:].isdigit()):
-                return(num)
-            else:
-                print("Неправильно набран номер!")
-                return get_num()
-        def get_name():
-            name = input("Введите имя: ")
-            name = name.title()
-            return name
-
-        d[get_name] = get_num
-
-        c = input("Всё? y/n")
-            if c == 'y':
-                break
-
-
-
-
-print(d)
 
