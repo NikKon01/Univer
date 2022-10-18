742898c62a004a2df20152d576be763a2ff8b03f
NikKon01 <4453431@gmail.com>
2022-10-18 10:58:49 +0300

    new

--- Tasks/Monti holl.py
+++ Tasks/Monti holl.py
@@ -1,13 +1,13 @@
 import random
-not_change = 0
-change = 0
+nch = 0
+ch = 0
 for i in range(100000):
     a = [0, 0, 1]
     choice = random.randint(0, 2)
     a.pop(choice)
     a.remove(0)
     if a[0] == 1:
-        not_change += 1
+        nch += 1
     else:
-        change += 1
-print(f'Вероятность если поменять выбор: {not_change/1000} %\nВероятность если не менять выбор: {change/1000} %')
+        ch += 1
+print(f'Вероятность если поменять выбор: {nch/1000} %\nВероятность если не менять выбор: {ch/1000} %')
