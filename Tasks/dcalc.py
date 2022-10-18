fd7b4f84c5b143eb3f891c0be197f5d0950b49e7
NikKon01 <4453431@gmail.com>
2022-10-18 12:19:01 +0300

    new

---
+++ Tasks/dcalc.py
@@ -0,0 +1,10 @@
+import dcalc
+from random import randint
+
+def create_list(*args, **kwargs):
+
+def get_coord():
+    a = randint(1*100000, 360*100000)
+    return a / 100000
+
+print(create_list(get_coord(), get_coord(), get_coord(), get_coord(), pole=get_coord(), put_1=get_coord()))
